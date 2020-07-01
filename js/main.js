4c599b18c90e882da8c23fe7a34a1ed3a06b8c62
irene-gomez <irene@irenegf.com>
2020-07-01 19:06:40 +0200

    Initialize class Calculator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'use strict';
+class Calculator {
+    constructor(currentOperandElement) {
+        this.currentOperand = currentOperandElement;
+        this.clearDisplay();
+    }
+    clearDisplay() {
+        this.currentOperand.innerHTML = '';
+    }
+    addNumber(number) {
+        this.currentOperand.innerHTML = this.currentOperand.innerHTML + number;
+    }
+}
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clea]');
+const equalButton = document.querySelector('[data-equal]');
+const currentOperandElement = document.querySelector('[data-operand]');
+const calculator = new Calculator(currentOperandElement);
+numberButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        calculator.addNumber(parseInt(element.innerHTML));
+    });
+});
+allClearButton.addEventListener('click', () => {
+    calculator.clearDisplay();
+});
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,36 @@
 'use strict';
+
+class Calculator {
+    currentOperand: any;
+
+    constructor(currentOperandElement: any) {
+        this.currentOperand = currentOperandElement;
+        this.clearDisplay();
+    }
+
+    clearDisplay() {
+        this.currentOperand.innerHTML = '';
+    }
+
+    addNumber(number: number) {
+        this.currentOperand.innerHTML = this.currentOperand.innerHTML + number;
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clea]');
+const equalButton = document.querySelector('[data-equal]');
+const currentOperandElement = document.querySelector('[data-operand]');
+
+const calculator = new Calculator(currentOperandElement);
+
+numberButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        calculator.addNumber(parseInt(element.innerHTML));
+    })
+});
+
+allClearButton.addEventListener('click', () => {
+    calculator.clearDisplay();
+});
