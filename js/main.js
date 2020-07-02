88613bb5a098def630b176bae2f17bd3e0d0abaa
irene-gomez <irene@irenegf.com>
2020-07-02 17:02:25 +0200

    Add updateDisplay method

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 'use strict';
 class Calculator {
     constructor(currentOperandElement) {
-        this.currentOperand = currentOperandElement;
+        this.currentOperandText = currentOperandElement;
         this.clearDisplay();
     }
     clearDisplay() {
-        this.currentOperand.innerHTML = '';
+        this.currentOperand = '';
     }
-    addNumber(number) {
-        this.currentOperand.innerHTML = this.currentOperand.innerHTML + number;
+    concatNumber(number) {
+        this.currentOperand = this.currentOperand + number;
+    }
+    updateDisplay() {
+        this.currentOperandText.innerText = this.currentOperand;
     }
 }
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
-const allClearButton = document.querySelector('[data-all-clea]');
+const allClearButton = document.querySelector('[data-all-clear]');
 const equalButton = document.querySelector('[data-equal]');
 const currentOperandElement = document.querySelector('[data-operand]');
 const calculator = new Calculator(currentOperandElement);
 numberButtons.forEach(element => {
     element.addEventListener('click', () => {
-        calculator.addNumber(parseInt(element.innerHTML));
+        calculator.concatNumber(parseInt(element.innerHTML));
+        calculator.updateDisplay();
     });
-});
-allClearButton.addEventListener('click', () => {
-    calculator.clearDisplay();
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,36 +1,43 @@
 'use strict';
 
 class Calculator {
+    currentOperandText: any;
     currentOperand: any;
 
     constructor(currentOperandElement: any) {
-        this.currentOperand = currentOperandElement;
+        this.currentOperandText = currentOperandElement;
         this.clearDisplay();
     }
 
     clearDisplay() {
-        this.currentOperand.innerHTML = '';
+        this.currentOperand = '';
     }
 
-    addNumber(number: number) {
-        this.currentOperand.innerHTML = this.currentOperand.innerHTML + number;
+    concatNumber(number: number) {
+        this.currentOperand = this.currentOperand + number;
+    }
+
+    updateDisplay() {
+        this.currentOperandText.innerText = this.currentOperand;
     }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
-const allClearButton = document.querySelector('[data-all-clea]');
+const allClearButton = document.querySelector('[data-all-clear]');
 const equalButton = document.querySelector('[data-equal]');
 const currentOperandElement = document.querySelector('[data-operand]');
 
-const calculator = new Calculator(currentOperandElement);
+const calculator: Calculator = new Calculator(currentOperandElement);
 
 numberButtons.forEach(element => {
     element.addEventListener('click', () => {
-        calculator.addNumber(parseInt(element.innerHTML));
+        calculator.concatNumber(parseInt(element.innerHTML));
+        calculator.updateDisplay();
     })
 });
 
-allClearButton.addEventListener('click', () => {
-    calculator.clearDisplay();
-});
+// allClearButton.addEventListener('click', button => {
+//     calculator.clearDisplay();
+//     calculator.updateDisplay();
+// });
